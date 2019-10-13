e345980d4852645908708f973f15b12efc59e5e4
KlykovVasiliy <Klykov_V@mail.ru>
2019-10-14 01:04:44 +0300

    Home work 6.4.5

--- 6 module/lessons_6_4/companyEmployees/src/Company.java
+++ 6 module/lessons_6_4/companyEmployees/src/Company.java
@@ -2,8 +2,26 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 public class Company {
-    private int incomeCompany;
+    private int incomeCompany = 0;
     private int countTopManager = 0;
+    private int countSalesMan = 0;
+
+    private void setCountTopManager(int count) {
+        this.countTopManager = count;
+    }
+
+    private void setCountSalesMan(int count) {
+        if (count > 0) {
+            this.countSalesMan = count;
+        }
+        if (count < 0) {
+            countSalesMan -= count;
+        }
+    }
+
+    private int getCountSalesMan() {
+        return countSalesMan;
+    }
 
     private ArrayList<AbstractEmployees> listEmployeesCompany = new ArrayList<>(270);
     private Comparator<AbstractEmployees> comparator = new ComparatorSalary().thenComparing
@@ -21,7 +39,6 @@ public class Company {
         for (int i = 0; i < countManager; i++) {
             TopManager topManager = new TopManager("topManager m" + i);
             listEmployeesCompany.add(topManager);
-            countTopManager++;
             for (int a = 1; a <= salesManager / countManager; a++) {
                 SalesMan salesMan = new SalesMan("salesMan s" + i + a);
                 listEmployeesCompany.add(salesMan);
@@ -32,25 +49,29 @@ public class Company {
                 listEmployeesCompany.add(cleark);
             }
         }
+        setCountTopManager(countManager);
+        setCountSalesMan(salesManager);
         setSalaryEmployees();
     }
 
     private void setSalaryEmployees() {
         for (AbstractEmployees ob : listEmployeesCompany) {
+            double deductionIncome;
             if (ob instanceof TopManager) {
+                deductionIncome = 40000.0;
                 int teenMillions = 10000000;
                 if (incomeCompany > teenMillions) {
                     double persentageOfIncomeCompany = 0.05;
-                    ob.setSalary(incomeCompany * persentageOfIncomeCompany /
-                            countTopManager + 40000.0);
-                } else {
-                    ob.setSalary(40000.0);
+                    deductionIncome = incomeCompany * persentageOfIncomeCompany /
+                            countTopManager + 40000.0;
                 }
+                ob.setSalary(deductionIncome);
             } else if (ob instanceof SalesMan) {
-                ob.setSalary(ob.getMonthSalary() + 30000.0);
-
+                deductionIncome = ob.getMonthSalary() + 30000.0;
+                ob.setSalary(deductionIncome);
             } else if (ob instanceof Clerk) {
-                ob.setSalary(25000.0);
+                deductionIncome = 25000.0;
+                ob.setSalary(deductionIncome);
             }
         }
         listEmployeesCompany.sort(comparator);
@@ -60,10 +81,40 @@ public class Company {
         return (int) (Math.random() * listEmployeesCompany.size());
     }
 
-    public void fireEmployees() {                                   //увольнение сотрудников
-        for (int i = 0; i < listEmployeesCompany.size() / 10; i++) {
-            listEmployeesCompany.remove(randomEmployeeSelection());
+    public void fireEmployees() {                                                                   //увольнение сотрудников
+        int removeCount = 0;
+        for (int i = 0; i < listEmployeesCompany.size(); i++) {
+            int removeEmployee = randomEmployeeSelection();
+            if (listEmployeesCompany.get(removeEmployee) instanceof SalesMan) {
+                if (!salesRevenueMoreThreshold()) {
+                    System.out.println("Менеджер по продажам не может быть уволен, иначе дохода " +
+                            "компании будет недостаточно для премирования топ менеджеров.");
+                } else {
+                    listEmployeesCompany.remove(removeEmployee);
+                    setCountSalesMan(-1);
+                    removeCount++;
+                }
+            } else {
+                listEmployeesCompany.remove(removeEmployee);
+                removeCount++;
+            }
+            if (removeCount >= listEmployeesCompany.size() / 10) {
+                System.out.printf("Увольнение 10 процентов сотрудников компании. " +
+                        "Уволено %d сотрудников.%n", removeCount);
+                break;
+            }
         }
+    }
+
+    private boolean salesRevenueMoreThreshold() {
+        double income = 0.0;
+        for (int i = 0; i < getCountSalesMan() - 1; i++) {
+            SalesMan salesMan = new SalesMan("salesMan " + i + " после увольнения");
+            income += salesMan.getSales();
+        }
+        double teenMillions = 10000000.0;
+        System.out.printf("Доход будет составлять %d%n", (int) income);
+        return teenMillions < income;
     }
 
     public void printListEmployeesCompany() {
@@ -85,7 +136,7 @@ public class Company {
     public ArrayList<Employee> getLowestSalaryStaff(int count) {
         ArrayList<Employee> list = new ArrayList<>();
         for (AbstractEmployees ob : listEmployeesCompany.subList(0, count - 1)) {
-                list.add((Employee) ob);
+            list.add((Employee) ob);
         }
         return list;
     }
