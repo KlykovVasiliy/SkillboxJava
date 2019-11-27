eada3ba1eacda56824de42ccdff6b604a0998cbe
KlykovVasiliy <Klykov_V@mail.ru>
2019-11-27 22:35:56 +0300

    Home work 7.1

---
+++ 7 module/LambdaExpressions/src/Main.java
@@ -0,0 +1,49 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import static java.util.Collections.*;
+
+public class Main {
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+        sort(staff, (o1, o2) -> {
+            int result = o1.getSalary().compareTo(o2.getSalary());
+            if (result == 0) {
+                return o1.getName().compareTo(o2.getName());
+            } else {
+                return result;
+            }
+        });
+
+        for (Employee employee : staff) {
+            System.out.println(employee);
+        }
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
